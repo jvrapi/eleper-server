9e1cf7ddf5a42b6094ef76b0051a61b2f95a8f01
João Vitor Santos <joaooviitorr@hotmail.com>
2021-04-22 12:26:53 -0300

    Prettier config updated; Added yup validation em post methods; Sender email in email service updated;

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import UserToken from '../models/UserToken';
@@ -10,115 +11,184 @@ import UserView from '../views/UserView';
 const userView = new UserView();
 
 class UserController {
-	async authentication(request: Request, response: Response) {
-		const { username, password } = request.body;
+  async authentication(request: Request, response: Response) {
+    const { username, password } = request.body;
 
-		const repository = getRepository(User);
-		const userTokenRepository = getRepository(UserToken);
+    const repository = getRepository(User);
+    const userTokenRepository = getRepository(UserToken);
 
-		try {
-			const user = await repository.findOne({
-				where: { email: username },
-			});
+    const data = { username, password };
 
-			if (user) {
-				const passwordIsValid = await bcrypt.compare(password, user.password);
+    const schema = Yup.object().shape({
+      username: Yup.string()
+        .email('Informe um usuario valido')
+        .required('Informe o usuario para realizar a autenticação'),
+      password: Yup.string().required('Informe a senha do usuario'),
+    });
 
-				if (!passwordIsValid) {
-					return response.status(401).json({ error: 'Senha inválida' });
-				}
+    try {
+      await schema.validate(data, { abortEarly: false });
+      const user = await repository.findOne({
+        where: { email: username },
+      });
 
-				const userToken = await userTokenRepository.findOne({
-					where: { userId: user.id },
-					relations: ['user'],
-				});
+      if (user) {
+        const passwordIsValid = await bcrypt.compare(password, user.password);
 
-				return response.json(userView.authentication(userToken as UserToken));
-			}
-			return response.status(404).json({ error: 'Usuario não encontrado' });
-		} catch (error) {
-			return response
-				.status(500)
-				.json({ error: 'Erro ao tentar autenticar o usuário' });
-		}
-	}
+        if (!passwordIsValid) {
+          return response.status(401).json({ error: 'Senha inválida' });
+        }
 
-	async save(request: Request, response: Response) {
-		const { name, email, cpf, password, birth } = request.body;
+        const userToken = await userTokenRepository.findOne({
+          where: { userId: user.id },
+          relations: ['user'],
+        });
 
-		const secretKey = process.env.SECRET_KEY;
+        return response.json(userView.authentication(userToken as UserToken));
+      }
+      return response.status(404).json({ error: 'Usuario não encontrado' });
+    } catch (err) {
+      const validationErrors: Record<string, string> = {};
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path as string] = error.message;
+        });
 
-		const userTokenRepository = getRepository(UserToken);
+        return response.status(500).json(validationErrors);
+      }
 
-		const repository = getRepository(User);
-		try {
-			const emailAlreadyExists = await repository.findOne({ where: { email } });
+      return response
+        .status(500)
+        .json({ error: 'Erro ao tentar autenticar o usuário' });
+    }
+  }
 
-			const cpfAlreadyExists = await repository.findOne({ where: { cpf } });
+  async save(request: Request, response: Response) {
+    const { name, email, cpf, password, birth } = request.body;
 
-			if (emailAlreadyExists) {
-				return response
-					.status(409)
-					.json({ error: 'E-mail já possui cadastro' });
-			}
+    const secretKey = process.env.SECRET_KEY;
 
-			if (cpfAlreadyExists) {
-				return response.status(409).json({ error: 'CPF já possui cadastro' });
-			}
-			const newUser = repository.create({
-				name,
-				email,
-				cpf: cpf.replace(/\D/g, ''),
-				password,
-				birth,
-			});
+    const userTokenRepository = getRepository(UserToken);
 
-			await repository.save(newUser);
+    const repository = getRepository(User);
 
-			const token = jwt.sign({ id: newUser.id }, secretKey || 'secret');
+    const data = { name, email, cpf, password, birth };
 
-			const userToken = userTokenRepository.create({
-				id: token,
-				userId: newUser.id,
-			});
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Informe o nome do usuário'),
+      cpf: Yup.string().required('Informe o cpf do usuário'),
+      birth: Yup.string().required('Informe a data de nascimento do usuário'),
+      email: Yup.string()
+        .email('Informe um e-mail válido')
+        .required('Informe o e-mail do usuario'),
+      password: Yup.string()
+        .min(6, 'A senha deve ter no mínimo 6 caracteres')
+        .required('Informe a senha do usuario'),
+    });
 
-			await userTokenRepository.save(userToken);
+    try {
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      const emailAlreadyExists = await repository.findOne({ where: { email } });
 
-			return response.status(201).json(userView.newUser(newUser, userToken.id));
-		} catch {
-			return response
-				.status(500)
-				.json({ error: 'Erro ao tentar salvar o usuário' });
-		}
-	}
+      const cpfAlreadyExists = await repository.findOne({ where: { cpf } });
 
-	async redefinePassword(request: Request, response: Response) {
-		const { email, password, code } = request.body;
+      if (emailAlreadyExists) {
+        return response
+          .status(409)
+          .json({ error: 'E-mail já possui cadastro' });
+      }
 
-		const repository = getRepository(User);
+      if (cpfAlreadyExists) {
+        return response.status(409).json({ error: 'CPF já possui cadastro' });
+      }
+      const newUser = repository.create({
+        name,
+        email,
+        cpf: cpf.replace(/\D/g, ''),
+        password,
+        birth,
+      });
 
-		try {
-			const user = await repository.findOne({ email });
+      await repository.save(newUser);
 
-			if (user?.code !== code) {
-				return response.status(500).json({ error: 'Código inválido' });
-			}
+      const token = jwt.sign({ id: newUser.id }, secretKey || 'secret');
 
-			const updateUser = repository.create({
-				...user,
-				code: null,
-				password,
-			});
+      const userToken = userTokenRepository.create({
+        id: token,
+        userId: newUser.id,
+      });
 
-			await repository.save(updateUser);
+      await userTokenRepository.save(userToken);
 
-			return response.json(userView.details(updateUser));
-		} catch {
-			return response
-				.status(500)
-				.json({ error: 'Erro ao tentar redefinir a senha' });
-		}
-	}
+      return response.status(201).json(userView.newUser(newUser, userToken.id));
+    } catch (err) {
+      const validationErrors: Record<string, string> = {};
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path as string] = error.message;
+        });
+
+        return response.status(500).json(validationErrors);
+      }
+
+      return response
+        .status(500)
+        .json({ error: 'Erro ao tentar salvar o usuário' });
+    }
+  }
+
+  async redefinePassword(request: Request, response: Response) {
+    const { email, password, code } = request.body;
+
+    const repository = getRepository(User);
+
+    const data = { email, password, code };
+
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email('Informe um email valido')
+
+        .required('Informe o email para redefinir a senha'),
+      password: Yup.string()
+        .min(6, 'A senha deve ter no mínimo 6 caracteres')
+        .required('Informe a nova senha '),
+      code: Yup.string()
+        .min(11, 'Código Inválido')
+        .max(11, 'Código Inválido')
+        .required('Informe o código para redefinir a senha'),
+    });
+
+    try {
+      await schema.validate(data, { abortEarly: false });
+      const user = await repository.findOne({ email });
+
+      if (user?.code !== code) {
+        return response.status(500).json({ error: 'Código inválido' });
+      }
+
+      const updateUser = repository.create({
+        ...user,
+        code: null,
+        password,
+      });
+
+      await repository.save(updateUser);
+
+      return response.json(userView.details(updateUser));
+    } catch (err) {
+      const validationErrors: Record<string, string> = {};
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path as string] = error.message;
+        });
+        return response.status(500).json(validationErrors);
+      }
+      return response
+        .status(500)
+        .json({ error: 'Erro ao tentar redefinir a senha' });
+    }
+  }
 }
-
 export default UserController;
--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -3,66 +3,66 @@ import handlebars from 'handlebars';
 import nodemailer, { Transporter } from 'nodemailer';
 
 interface Variables {
-	name: string;
-	title: string;
-	description: string;
-	user_id: string;
-	code: string;
+  name: string;
+  title: string;
+  description: string;
+  user_id: string;
+  code: string;
 }
 
 class SendMailService {
-	private client: Transporter;
+  private client: Transporter;
 
-	constructor() {
-		/*Create test account for send e-mail */
-		nodemailer.createTestAccount().then((account) => {
-			const transporter = nodemailer.createTransport({
-				host: account.smtp.host,
-				port: account.smtp.port,
-				secure: account.smtp.secure,
-				auth: {
-					user: account.user,
-					pass: account.pass,
-				},
-			});
+  constructor() {
+    /*Create test account for send e-mail */
+    nodemailer.createTestAccount().then((account) => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
 
-			this.client = transporter;
-		});
-	}
+      this.client = transporter;
+    });
+  }
 
-	async execute(
-		to: string,
-		subject: string,
-		variables: Variables,
-		path: string
-	) {
-		let html;
-		try {
-			/* Load file */
-			const templateFileContent = fs.readFileSync(path).toString('utf8');
+  async execute(
+    to: string,
+    subject: string,
+    variables: Variables,
+    path: string
+  ) {
+    let html;
+    try {
+      /* Load file */
+      const templateFileContent = fs.readFileSync(path).toString('utf8');
 
-			/*Convert file */
-			const mailTemplateParse = handlebars.compile(templateFileContent);
+      /*Convert file */
+      const mailTemplateParse = handlebars.compile(templateFileContent);
 
-			/*Add the variables in template file */
-			html = mailTemplateParse(variables);
-		} catch {
-			throw new Error('Erro ao tentar carregar o arquivo de template');
-		}
-		try {
-			const message = await this.client.sendMail({
-				to,
-				subject,
-				html,
-				from: 'EHR <noreplay@ehr.com.br>',
-			});
-			console.log('Message sent: %s', message.messageId);
-			console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
-			return message;
-		} catch {
-			throw new Error('Erro ao tentar enviar o email');
-		}
-	}
+      /*Add the variables in template file */
+      html = mailTemplateParse(variables);
+    } catch {
+      throw new Error('Erro ao tentar carregar o arquivo de template');
+    }
+    try {
+      const message = await this.client.sendMail({
+        to,
+        subject,
+        html,
+        from: 'EHR <noreplay@eleper.com.br>',
+      });
+      console.log('Message sent: %s', message.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+      return message;
+    } catch {
+      throw new Error('Erro ao tentar enviar o email');
+    }
+  }
 }
 
 export default new SendMailService();
