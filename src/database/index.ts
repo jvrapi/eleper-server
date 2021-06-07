f458c03388afc2c853ea7b2d24cc17450b7a1e67
João Vitor Santos <joaooviitorr@hotmail.com>
2021-06-07 11:00:10 -0300

    Docker compose config updated; Updated data files; Database connection file updated;

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,60 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 
-createConnection();
+import Disease from '../models/Disease';
+import Medicine from '../models/Medicine';
+import Surgery from '../models/Surgery';
+import diseases from '../utils/diseases';
+import medicines from '../utils/medicines';
+import surgeries from '../utils/surgeries';
+
+interface JsonData {
+	name: string;
+}
+
+async function connectToDatabase() {
+	const connection = await createConnection();
+	await verifyDiseases(connection);
+	await verifySurgeries(connection);
+	await verifyMedicines(connection);
+}
+
+async function verifyDiseases(connection: Connection) {
+	const repository = await connection.getRepository(Disease);
+	diseases.forEach(async (disease: JsonData) => {
+		const diseaseAlreadyExists = await repository.findOne({
+			name: disease.name,
+		});
+		if (!diseaseAlreadyExists) {
+			const newDisease = repository.create({ name: disease.name });
+			await repository.save(newDisease);
+		}
+	});
+}
+
+async function verifySurgeries(connection: Connection) {
+	const repository = await connection.getRepository(Surgery);
+	surgeries.forEach(async (surgery) => {
+		const surgeryAlreadyExists = await repository.findOne({
+			name: surgery.name,
+		});
+		if (!surgeryAlreadyExists) {
+			const newSurgery = repository.create({ name: surgery.name });
+			await repository.save(newSurgery);
+		}
+	});
+}
+
+async function verifyMedicines(connection: Connection) {
+	const repository = await connection.getRepository(Medicine);
+	medicines.forEach(async (medicine: JsonData) => {
+		const medicineAlreadyExists = await repository.findOne({
+			name: medicine.name,
+		});
+		if (!medicineAlreadyExists) {
+			const newMedicine = repository.create({ name: medicine.name });
+			await repository.save(newMedicine);
+		}
+	});
+}
+
+connectToDatabase();
---
+++ src/utils/surgeries.ts
@@ -0,0 +1,265 @@
+export default [
+	{
+		name: 'Adenoidectomia',
+	},
+	{
+		name: 'Amigdalectomia',
+	},
+	{
+		name: 'Apendicectomia',
+	},
+	{
+		name: 'Artrodese de médias / grandes articulacões de membro inferior',
+	},
+	{
+		name: 'Artroplastia de ressecção de média / grande articulação',
+	},
+	{
+		name: 'Capsulotomia yag-laser',
+	},
+	{
+		name: 'Colecistectomia',
+	},
+	{
+		name: 'Colecistectomia videolaparoscópica',
+	},
+	{
+		name: 'Coledocotomia com ou sem colecistectomia',
+	},
+	{
+		name: 'Colpoperineoplastia anterior e posterior',
+	},
+	{
+		name: 'Colpoperineoplastia anterior e posterior com amputação de colo',
+	},
+	{
+		name: 'Conização',
+	},
+	{
+		name: 'Correção de hipospadia (primeiro tempo)',
+	},
+	{
+		name: 'Correção de hipospadia (segundo tempo)',
+	},
+	{
+		name: 'Curetagem semiótica com ou sem dilatação do colo uterino',
+	},
+	{
+		name: 'Excisão de lesão / tumor ano-retal',
+	},
+	{
+		name: 'Excisão e sutura de lesão da boca',
+	},
+	{
+		name: 'Exerese de cisto sacro-coccígeo',
+	},
+	{
+		name: 'Exerese de glândula de Bartholin / Skene',
+	},
+	{
+		name: 'Exerese de nódulo de mama',
+	},
+	{
+		name:
+			'Exploração articular com ou sem sinovectomia de médias / grandes articulações',
+	},
+	{
+		name: 'Fasciectomia',
+	},
+	{
+		name: 'Facectomia com implante de lente intra-ocular',
+	},
+	{
+		name: 'Facoemulsificação com implante de lente intra-ocular rígida',
+	},
+	{
+		name: 'Facoemulsificação com implante de lente intra-ocular dobrável',
+	},
+	{
+		name: 'Fistulectomia ou fistulotomia anal',
+	},
+	{
+		name: 'Fotocoagulação a laser',
+	},
+	{
+		name: 'Fototrabeculoplastia a laser',
+	},
+	{
+		name: 'Hemorroidectomia',
+	},
+	{
+		name: 'Herniorrafia umbilical',
+	},
+	{
+		name: 'Herniorrafia epigástrica',
+	},
+	{
+		name: 'Herniorrafia incisional',
+	},
+	{
+		name: 'Herniorrafia recidivante',
+	},
+	{
+		name: 'Hernioplastia inguinal/crural (bilateral)',
+	},
+	{
+		name: 'Hernioplastia inguinal/crural (unilateral)',
+	},
+	{
+		name: 'Histerectomia total',
+	},
+	{
+		name: 'Histerectomia (por via vaginal)',
+	},
+	{
+		name: 'Histerectomia com anexectomia uni ou bilateral',
+	},
+	{
+		name: 'Histeroscopia cirúrgica com ressectoscópio',
+	},
+	{
+		name: 'Implante de prótese anti-glaucomatosa',
+	},
+	{
+		name: 'Iridotomia a laser',
+	},
+	{
+		name: 'Laqueadura tubária',
+	},
+	{
+		name: 'Mastectomia simples',
+	},
+	{
+		name: 'Mastoidectomia subtotal',
+	},
+	{
+		name: 'Miomectomia videolaparoscópica',
+	},
+	{
+		name: 'Ooforectomia/ooforoplastia',
+	},
+	{
+		name: 'Orquidopexia unilateral',
+	},
+	{
+		name: 'Pan-fotocoagulação retiniana a laser',
+	},
+	{
+		name: 'Plástica mamária feminina nao estética',
+	},
+	{
+		name: 'Postectomia',
+	},
+	{
+		name: 'Prostatectomia suprapúbica',
+	},
+	{
+		name: 'Reparação outras hérnias (inclui herniorrafia muscular)',
+	},
+	{
+		name: 'Ressecção endoscópica de próstata',
+	},
+	{
+		name: 'Retirada de tracão trans-esquelética',
+	},
+	{
+		name:
+			'Retração cicatricial dos dedos com comprometimento tendinoso (por dedo)',
+	},
+	{
+		name: 'Segmentectomia de mama',
+	},
+	{
+		name: 'Septoplastia para correção de desvio',
+	},
+	{
+		name: 'Tenólise',
+	},
+	{
+		name: 'Tenomiorrafia',
+	},
+	{
+		name: 'Tenoplastia ou enxerto de tendão único',
+	},
+	{
+		name: 'Termoterapia transpupilar',
+	},
+	{
+		name: 'Timpanoplastia (unilateral/bilateral)',
+	},
+	{
+		name: 'Tiroidectomia parcial',
+	},
+	{
+		name: 'Tratamento cirúrgico de varizes (bilateral)',
+	},
+	{
+		name: 'Tratamento cirúrgico de varizes (unilateral)',
+	},
+	{
+		name: 'Tratamento cirúrgico da incontinência urinária por via vaginal',
+	},
+	{
+		name:
+			'Tratamento cirúrgico de infecção pós-artroplastia (grandes articulações)',
+	},
+	{
+		name: 'Tratamento cirúrgico da hidrocele',
+	},
+	{
+		name: 'Tratamento cirúrgico da incontinência urinária via abdominal',
+	},
+	{
+		name:
+			'Tratamento cirúrgico da rotura de menisco - meniscectomia parcial ou total',
+	},
+	{
+		name: 'Tratamento cirúrgico da varicocele',
+	},
+	{
+		name: 'Tratamento cirúrgico de dedo em martelo / em garra (mão e pé)',
+	},
+	{
+		name:
+			'Tratamento cirúrgico de fratura viciosamente consolidada dos ossos longosexceto da mão e do pé',
+	},
+	{
+		name: 'Tratamento cirúrgico de retração muscular',
+	},
+	{
+		name: 'Tratamento cirúrgico do glaucoma congênito',
+	},
+	{
+		name:
+			'Tratamento cirúrgico do halux valgus sem osteotomia do primeiro osso metatársico',
+	},
+	{
+		name: 'Tratamento cirúrgico do pé torto congênito',
+	},
+	{
+		name:
+			'Tratamento cirúrgico da síndrome compressiva em túnel ósteo-fibroso ao nível do carpo',
+	},
+	{
+		name: 'Turbinectomia',
+	},
+	{
+		name: 'Vasectomia parcial ou completa',
+	},
+	{
+		name:
+			'Videoscopia para diagnóstico, drenagem, toalete ou biópsia ou remoção delesões',
+	},
+	{
+		name: 'Videoscopia para síntese, reconstrução ou transplante de estruturas',
+	},
+	{
+		name: 'Vitrectomia posterior',
+	},
+	{
+		name: 'Vitrectomia posterior com infusão de óleo de silicone e endolaser',
+	},
+	{
+		name: 'Vitrectomia posterior com infusão de perfluorcarbono e endolaser',
+	},
+];
